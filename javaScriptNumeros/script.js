f26916095c43706712369ad0320ccf03a0bba496
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-01-02 18:46:34 +0100

    metodos number

---
+++ javaScriptNumeros/script.js
@@ -0,0 +1,18 @@
+
+// - Una variable que contenga tu altura en centímetros (entero)
+const miAlturacm = 170; 
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+const miAlturaMetros = 1.7;
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+const pesoKg = 59.5;
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+const miAlturaMRedondeadaArriba = Math.round(miAlturaMetros);
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+const miPesoRedondeadoAbajo = Math.floor(pesoKg);
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+const sumar1AlMaximo = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
